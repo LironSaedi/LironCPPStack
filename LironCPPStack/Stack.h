453d1520ff9e86795f27e32beb51abc8c3da723e
Liron Saedi <Liron.Saedi@GMR.local>
2023-06-30 19:12:11 -0700

    Initial Commit

---
+++ LironCPPStack/Stack.h
@@ -0,0 +1,49 @@
+#pragma once
+template<typename T>
+class Stack
+{
+private:
+	int Count = 10;
+	T* array[Count];
+
+
+	T* new array
+	void Resize(int size)
+	{
+		
+	}
+
+public:
+	int current = 0;
+	Stack();
+	~Stack();
+
+	void Push(T value)
+	{
+		for (size_t i = 0; i < Count; i++)
+		{
+
+		}
+	}
+
+	T Pop()
+	{
+		auto thing = array[3];
+	}
+
+	T Peek()
+	{
+
+	}
+
+	
+		/*
+		const int size = 10;
+		int array[];
+		void Push(int val)
+		{
+			array[] = new int[size];
+		}
+		*/
+};
+
